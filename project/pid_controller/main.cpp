9c8c4f6cc1617f8535ad447276e3f71333339e3d
Damiano Muzzolon <damianomuzzolon97@gmail.com>
2024-06-09 19:14:58 +0200

    Implement PID controller for steering

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -299,8 +299,8 @@ int main() {
       /**
        * TODO (step 3): uncomment these lines
        **/
-      //           // Update the delta time with the previous command
-      //           pid_steer.UpdateDeltaTime(new_delta_time);
+      // Update the delta time with the previous command
+      pid_steer.UpdateDeltaTime(new_delta_time);
 
       // Compute steer error
       double error_steer;
@@ -311,24 +311,25 @@ int main() {
        * TODO (step 3): compute the steer error (error_steer) from the position
        *and the desired trajectory
        **/
-      //           error_steer = 0;
+      const auto theta_ref = angle_between_points(
+          x_position, y_position, x_points.back(), y_points.back());
+      error_steer = yaw - theta_ref;
 
       /**
        * TODO (step 3): uncomment these lines
        **/
-      //           // Compute control to apply
-      //           pid_steer.UpdateError(error_steer);
-      //           steer_output = pid_steer.TotalError();
+      // Compute control to apply
+      pid_steer.UpdateError(error_steer);
+      steer_output = pid_steer.TotalError();
 
-      //           // Save data
-      //           file_steer.seekg(std::ios::beg);
-      //           for(int j=0; j < i - 1; ++j) {
-      //               file_steer.ignore(std::numeric_limits<std::streamsize>::max(),
-      //               '\n');
-      //           }
-      //           file_steer  << i ;
-      //           file_steer  << " " << error_steer;
-      //           file_steer  << " " << steer_output << endl;
+      // Save data
+      file_steer.seekg(std::ios::beg);
+      for (int j = 0; j < i - 1; ++j) {
+        file_steer.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      }
+      file_steer << i;
+      file_steer << " " << error_steer;
+      file_steer << " " << steer_output << endl;
 
       ////////////////////////////////////////
       // Throttle control
