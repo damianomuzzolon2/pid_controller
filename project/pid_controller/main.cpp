34c53a38bea614e576eda4a7d2bf4897075f1183
Damiano Muzzolon <damianomuzzolon97@gmail.com>
2024-06-15 16:34:49 +0200

    Tune PIDs

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -231,11 +231,11 @@ int main() {
 
   const double max_limit_steer{1.2};
   const double min_limit_steer{-1.2};
-  pid_steer.Init(1.0, 1.0, 1.0, max_limit_steer, min_limit_steer);
+  pid_steer.Init(0.3, 0.01, 0.25, max_limit_steer, min_limit_steer);
 
   const double max_limit_throttle{1};
   const double min_limit_throttle{-1};
-  pid_throttle.Init(1.0, 1.0, 1.0, max_limit_throttle, min_limit_throttle);
+  pid_throttle.Init(0.1, 0.006, 0.001, max_limit_throttle, min_limit_throttle);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer,
                &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char* data,
